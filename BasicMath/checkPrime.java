de86773b33de1dbc7ce8a878d668706edc555403
Aditya Srivastava <adityaji682@gmail.com>
2023-07-06 23:43:23 +0530

    another Questions

--- BasicMath/checkPrime.java
+++ BasicMath/checkPrime.java
@@ -3,8 +3,26 @@ package BasicMath;
 import java.util.Scanner;
 
 public class checkPrime {
+    public static void checkPrime(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+                if ((n / i) != i) {
+                    count++;
+                }
+            }
+        }
+        if (count == 2) {
+            System.out.println("Prime number.");
+        } else {
+            System.out.println("Not a prime number.");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         int n = read.nextInt();
+        checkPrime(n);
     }
 }
--- BasicMath/gCd.java
+++ BasicMath/gCd.java
@@ -3,9 +3,18 @@ package BasicMath;
 import java.util.*;
 
 public class gCd {
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        int n = read.nextInt();
+        int a = read.nextInt();
+        int b = read.nextInt();
+        System.out.println("The GCD of the two numbers is :" + gcd(a, b));
 
     }
 }
