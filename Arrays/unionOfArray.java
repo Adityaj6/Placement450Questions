87c7981be6fbb124b01c073018dc5ea836f619a2
Aditya Srivastava <adityaji682@gmail.com>
2023-08-28 14:47:35 +0530

    Optimal solution of union of Array

--- Arrays/unionOfArray.java
+++ Arrays/unionOfArray.java
@@ -20,10 +20,46 @@ public class unionOfArray {
         return union;
     }
 
+    public static ArrayList<Integer> OptimalApproch(int arr1[], int arr2[]) {
+        int i = 0, j = 0;
+        ArrayList<Integer> union = new ArrayList<>();
+        int n = arr1.length;
+        int m = arr2.length;
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                if (union.size() == 0 || union.get(union.size() - 1) != arr1[i]) {
+                    union.add(arr1[i]);
+                    i++;
+
+                }
+
+            } else {
+                if (union.size() == 0 || union.get(union.size() - 1) != arr2[j]) {
+                    union.add(arr2[j]);
+                    j++;
+                }
+            }
+        }
+        while (i < n) {
+            if (union.get(union.size() - 1) != arr1[i]) {
+                union.add(arr1[i]);
+                i++;
+            }
+        }
+        while (j < m) {
+            if (union.get(union.size() - 1) != arr2[j]) {
+                union.add(arr2[j]);
+                j++;
+            }
+        }
+        return union;
+    }
+
     public static void main(String[] args) {
         int arr1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int arr2[] = { 2, 3, 4, 4, 5, 11, 12 };
-        ArrayList<Integer> Union = bruteForce(arr1, arr2);
+        // ArrayList<Integer> Union = bruteForce(arr1, arr2);
+        ArrayList<Integer> Union = OptimalApproch(arr1, arr2);
         System.out.println("Union of arr1 and arr2 is ");
         for (int val : Union)
             System.out.print(val + " ");
